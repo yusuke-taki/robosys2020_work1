9434d9c9a3ab56967d83095b71a2326ac9c9d167
yusuke-taki <72942000+yusuke-taki@users.noreply.github.com>
2020-12-05 22:24:35 +0900

    Update myled.c

--- myled.c
+++ myled.c
@@ -1,3 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
+
+/*
+ * Copyright(C) 2020 YUsuke Taki and Ryuichi Ueda.All right reserved.
+*/
+
 #include<linux/module.h>
 #include<linux/fs.h>
 #include<linux/cdev.h>
